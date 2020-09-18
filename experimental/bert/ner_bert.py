45809ba35ed81e8ff2578a45abbbccd605dcd9e7
Tim Attwell <tim.attwell@hotmail.co.uk>
2020-09-18 16:57:09 +0100

    .

--- experimental/bert/ner_bert.py
+++ experimental/bert/ner_bert.py
@@ -402,7 +402,7 @@ while cont == True:
         def take_second(elem):
             return elem[1]
         print("Pairs\n" + str(sorted(list(set(zip(art_ents, wordfreq))),key=take_second,reverse=True)))
-        print("Found over "+c+" articles.")
+        print("Found over "+str(c)+" articles.")
 
 
 
